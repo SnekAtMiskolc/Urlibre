f01a1afdb3b766832872b6b71d7a30338eebe425
Susa Milán <smlan@cock.li>
2023-01-01 17:25:33 +0100

    NewURL and intoURL for public api

--- models/url.go
+++ models/url.go
@@ -1,7 +1,28 @@
 package models
 
+import "time"
+
+// Used to represent a url!
 type URL struct {
 	ID      string `json:"id" bson:"id"`
 	Url     string `json:"url" bson:"url"`
 	Expires int64  `json:"exp" bson:"exp"`
+}
+
+// Used to represent the creation of a url!
+type NewURL struct {
+	Url     string `json:"url"`
+	Expires int    `json:"exp"`
+}
+
+func (nu *NewURL) IntoURL() *URL {
+	return &URL{
+		ID:      "asd",
+		Url:     nu.Url,
+		Expires: addDays(nu.Expires),
+	}
+}
+
+func addDays(days int) int64 {
+	return time.Now().AddDate(0, 0, days).Unix()
 }
